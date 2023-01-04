87b233c981619b739e63b090c46bfa11bfa4105e
akihiro <aponmaru@gmail.com>
2023-01-04 16:37:19 +0900

    [add] cdate でカレンダーを表示してみた

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,38 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+import { cdate } from "cdate"
+
+const now = cdate().tz("Asia/Tokyo")
+const start = now.startOf("month").startOf("week")
+const end = now.endOf("month").endOf("week")
+
+const weeks:string[][] = [];
+for (let day = start; +day < +end;) {
+  const week = []
+  for (let i = 0; i < 7; i++) {
+    week.push(day.format("YYYY/MM/DD"))
+    day = day.next("day")
+  }
+  weeks.push(week)
+}
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
+  <h1>{{ now.format("MMMM YYYY") }}</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <table>
+    <tr
+      v-for="week in weeks">
+      <td
+        v-for="day in week"
+        :class="{ 'text-red': day === now.format('YYYY/MM/DD') }">
+        {{ cdate(day).format("DD") }}
+      </td>
+    </tr>
+  </table>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.text-red {
+  color: red;
 }
 </style>
